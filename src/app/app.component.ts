248efef58749f4931377423f7240131b9b030130
Noelia M <mm.noe2011@gmail.com>
2020-05-18 18:08:17 +0200

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-examples-pipes';
+
+  nombre:string    ="albert einstein";
+  nombre2:string ="nOEliA muñOZ"
+  array:number[]   =[1,2,3,4,5,6,7,8,9,10];
+  PI:number        =Math.PI;
+  porcentaje:number=0.234;
+  salario:number   =12345;
+  fecha:Date       =new Date();
+  idioma:string='es';
+  videoUrl:string="https://www.youtube.com/embed/5vTqK25umK4"
+  activar:boolean=true;
+
+  animal={
+      nombre:'Misi',
+      tipo:'gato',
+      edad:4,
+      cualidades:{
+        pelo:'corto',
+        raza:'siamés'
+      }
+}
+
+  valorPromesa=new Promise<string>((resolve)=>{
+    setTimeout(()=>{
+      resolve('Datos recibidos');
+    },4500);
+  });
+
+
+
+
+
 }
---
+++ src/app/pipes/password.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'password'
+})
+export class PasswordPipe implements PipeTransform {
+
+  transform(value: string, activar:boolean=true): string {
+
+    return (activar) ? '*'.repeat(value.length) :value;
+  }
+
+}
